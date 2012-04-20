7d92b80f39e8e8bf1192fef7c5c4dcae3fcd63ec
David Cain <davidjosephcain@gmail.com>
2012-04-20 13:58:53 -0400

    Add a script to convert saved numpy images to png
    The script takes .npy paths as arguments, and converts each to a .png
    image, changing the file extension to png. Pre-existing images will be
    over-written.

---
+++ save_npy.py
@@ -0,0 +1,47 @@
+#!/usr/bin/env python
+
+# David Cain
+# Justin Sperry
+# 2012-04-18
+# CS365, Brian Eastwood
+
+"""
+    A script to convert .npy images to .png
+"""
+
+import os
+import sys
+
+import Image
+import numpy as np
+
+def convert(fn):
+    """
+        Converts the numpy image saved to 'fn' into a .png
+        The resulting image is saved with the same filename, except with
+        the .png extension (input image must have extension .npy).
+    """
+    assert fn[-4:] == "npy", "File extension should match '.npy'"
+
+    print "Converting file '%s' to .png" % fn
+    numpy_img = np.load(fn)
+
+    #Rescale to 0-255 and convert to uint8
+    rescaled = (255.0 / numpy_img.max() * (numpy_img - numpy_img.min())).astype(np.uint8)
+
+    im = Image.fromarray(rescaled)
+    im.save(fn[:-4] + ".png")
+
+
+if __name__ == "__main__":
+    """
+        Sys args should be paths to .npy files
+    """
+    usage = """
+    Usage: python save_npy.py file1 [file2] [file3]...
+    """
+    if len(sys.argv) <= 1:
+        print usage
+    for filename in sys.argv[1:]:
+        assert os.path.isfile(filename), "File '%s' doesn't exist" % filename
+        convert(filename)
